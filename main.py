8d672c659409a7135fac35093c162ccbb8b07ff0
u03n0 <u03n0@proton.me>
2023-11-04 00:08:27 +0100

    models and more

---
+++ main.py
@@ -0,0 +1,42 @@
+import torch
+import numpy as np
+from torch.utils.data import  DataLoader, SubsetRandomSampler
+from models import LanguageDataset, GenderedLSTM
+
+
+if __name__ == "__main__":
+    with open("french.csv") as f:
+        text = f.readlines()
+
+    # Hyperparameters model
+    vocab_size = 70
+    window_size = 10
+    embedding_dim = 2
+    hidden_dim = 16
+    dense_dim = 32
+    n_layers = 1
+    max_norm = 2
+
+    # Training config
+    train_val_split = 0.8
+    batch_size = 128
+    random_state = 13
+
+    torch.manual_seed(random_state)
+
+    dataset = LanguageDataset(text)
+    n_epochs = dataset.longest_noun
+
+    n_samples = len(dataset)
+    split_idx = int(n_samples * train_val_split)
+
+    train_indices, val_indices = np.arange(split_idx), np.arange(split_idx, n_samples)
+
+    train_dataloader = DataLoader(
+                dataset, sampler=SubsetRandomSampler(train_indices), batch_size=batch_size
+        )
+    val_dataloader = DataLoader(
+                dataset, sampler=SubsetRandomSampler(val_indices), batch_size=batch_size
+    )
+
+    print(dataset.labels)
---
+++ models.py
@@ -0,0 +1,77 @@
+from collections import defaultdict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset
+
+
+class LanguageDataset(Dataset):
+    """ Custom Language dataset 
+    
+    """
+
+    def __init__(self, data):
+        self.data = data
+        self.char2idx = {}
+        self.labels = []
+        self.longest_noun = 0
+
+        idx = 0
+        for word in self.data:
+            word_list = word.split(",")
+            noun = word_list[1]
+            if len(noun) > self.longest_noun:
+                self.longest_noun = len(noun)
+            label = word_list[2]
+            for char in noun:
+                if char not in self.char2idx:
+                    self.char2idx[char] = idx
+                    idx +=1
+            if label not in self.labels:
+                self.labels.append(label)
+        self.idx2char = {v: k for k, v in self.char2idx.items()}
+        
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        pass
+
+
+
+class GenderedLSTM(nn.Module):
+
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+        super(GenderedLSTM, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.vocab_size = vocab_size
+        self.tagset_size = tagset_size
+        self.allocate_params()
+
+
+    def allocate_params(self):
+
+        self.word_embeddings = nn.Embedding(self.vocab_size, self.embedding_dim)
+
+        self.lstm = nn.LSTM(self.embedding_dim, self.hidden_dim)
+
+        self.hidden2tag = nn.Linear(self.hidden_dim, self.tagset_size)
+
+
+    def foward(self, sequence):
+        embeds = self.word_embeddings(sequence)
+        lstm_out, _ = self.lstm(embeds.view(len(sequence), 1, -1))
+        tag_space = self.hidden2tag(lstm_out.view(len(sequence), -1))
+        tag_score = F.log_softmax(tag_space, dim=1)
+        return tag_score
+    
+    def train(self, learning_rate=0.1):
+
+        loss_function = nn.NLLLoss()
+        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        # with torch.no_grad():
+        #     inputs = 
